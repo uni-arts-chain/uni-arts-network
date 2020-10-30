da8a3063746e1972410e2cf98f95741aaeb91faa
terry.tu <tuminfei1981@gmail.com>
2020-10-30 15:01:18 +0800

    add types.json

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -1,7 +1,7 @@
 pub mod currency {
     use primitives::Balance;
 
-    pub const UART: Balance = 1_000_000_000_000_000_000;
+    pub const UART: Balance = 1_000_000_000_000;
     pub const DOLLARS: Balance = 100;
     pub const CENTS: Balance = DOLLARS / 100;
     pub const MILLICENTS: Balance = DOLLARS / 1_000;

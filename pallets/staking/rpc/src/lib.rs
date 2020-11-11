f3db8b84eca79b0304937f9b5f0315717a568a0e
yxf <yxf4559@163.com>
2020-11-11 18:16:16 +0800

    Fixed Balance json serde

--- pallets/staking/rpc/src/lib.rs
+++ pallets/staking/rpc/src/lib.rs
@@ -1,19 +1,20 @@
 use std::sync::Arc;
-use codec::{Codec, HasCompact};
+use codec::{Codec};
 use jsonrpc_derive::rpc;
 use jsonrpc_core::{Error as RpcError, ErrorCode, Result};
 use sp_blockchain::HeaderBackend;
 use sp_runtime::{generic::BlockId, traits::{Block as BlockT, MaybeDisplay, MaybeFromStr}};
 use sp_api::ProvideRuntimeApi;
+use pallet_staking_rpc_runtime_api::Reward;
 pub use pallet_staking_rpc_runtime_api::StakingApi as StakingRuntimeApi;
 
 #[rpc]
-pub trait StakingApi<AccountId, Balance> {
+pub trait StakingApi<AccountId, ResponseType> {
 	#[rpc(name = "staking_pendingRewards")]
 	fn pending_rewards(
 		&self, 
 		account_id: AccountId
-	) -> Result<Balance>;
+	) -> Result<ResponseType>;
 
 	#[rpc(name = "staking_poolAccountId")]
 	fn pool_account_id(&self) -> Result<AccountId>;
@@ -31,24 +32,27 @@ impl<C, P> Staking<C, P> {
 }
 
 
-impl<C, Block, AccountId, Balance> StakingApi<AccountId, Balance>
+impl<C, Block, AccountId, Balance> StakingApi<AccountId, Reward<Balance>>
 	for Staking<C, Block>
 where
 	Block: BlockT,
 	C: Send + Sync + 'static + ProvideRuntimeApi<Block> + HeaderBackend<Block>,
 	C::Api: StakingRuntimeApi<Block, AccountId, Balance>,
-	Balance: Codec + MaybeDisplay + MaybeFromStr + HasCompact, 
+	Balance: Codec + MaybeDisplay + MaybeFromStr + std::default::Default + std::fmt::Debug, 
 	AccountId: Codec
 {
 
-	fn pending_rewards(&self, account_id: AccountId) -> Result<Balance> {
+	fn pending_rewards(&self, account_id: AccountId) -> Result<Reward<Balance>> {
 		let api = self.client.runtime_api();
 		let at = BlockId::hash(self.client.info().best_hash);
 		api.pending_rewards(&at, 0, account_id).map_err(|e| RpcError {
 			code: ErrorCode::InternalError,
 			message: "Unable to query pending rewards".into(),
 			data: Some(format!("{:?}", e).into()),
-		})
+		}).map(|value| Reward(value))
+
+		// println!("balance = {:?}", balance);
+		// Ok(Reward { value: Default::default() })	
 	}
 
 	fn pool_account_id(&self) -> Result<AccountId> {

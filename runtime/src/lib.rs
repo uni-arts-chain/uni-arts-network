cef23dcdcb80ee8c857c5b7f7850023bb6dff80c
terry.tu <tuminfei1981@gmail.com>
2020-11-25 11:58:27 +0800

    add pay fee

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -103,7 +103,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("uart"),
 	impl_name: create_runtime_str!("uart"),
 	authoring_version: 1,
-	spec_version: 4,
+	spec_version: 5,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,

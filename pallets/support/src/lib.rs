6eeab3f7e73fbd2100429990e22e20ed4bd3ab9a
terry.tu <tuminfei1981@gmail.com>
2021-04-13 15:53:36 +0800

    add support

--- pallets/support/src/lib.rs
+++ pallets/support/src/lib.rs
@@ -1,9 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![allow(clippy::upper_case_acronyms)]
 
-use codec::{Decode, Encode, FullCodec, HasCompact};
-use sp_core::H160;
-use sp_runtime::{DispatchResult, FixedU128, RuntimeDebug,};
+use sp_runtime::{DispatchResult};
 use uniarts_primitives::AccountId;
 
 pub trait NftManager {

a219c1c2a50b537f4ef4991661d5312e91138911
terry.tu <tuminfei1981@gmail.com>
2021-04-13 18:42:25 +0800

    add support

--- pallets/support/src/lib.rs
+++ pallets/support/src/lib.rs
@@ -4,8 +4,8 @@
 use sp_runtime::{DispatchResult};
 use uniarts_primitives::AccountId;
 
-pub trait NftManager {
-    fn transfer_nft(collection_id: u64, item_id: u64, sender: AccountId, new_owner: AccountId) -> DispatchResult;
-    fn transfer_fungible(collection_id: u64, item_id: u64, value: u64, sender: AccountId, new_owner: AccountId) -> DispatchResult;
-    fn transfer_refungible(collection_id: u64, item_id: u64, value: u64, sender: AccountId, new_owner: AccountId) -> DispatchResult;
+pub trait NftManager<AccountId> {
+    fn transfer_nft(collection_id: u64, item_id: u64, sender: &AccountId, new_owner: &AccountId) -> DispatchResult;
+    fn transfer_fungible(collection_id: u64, item_id: u64, value: u64, sender: &AccountId, new_owner: &AccountId) -> DispatchResult;
+    fn transfer_refungible(collection_id: u64, item_id: u64, value: u64, sender: &AccountId, new_owner: &AccountId) -> DispatchResult;
 }

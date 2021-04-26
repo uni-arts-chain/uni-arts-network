8fcabecc6dfe1c68b5eaf06fcdf9a80a3c6495d2
terry.tu <tuminfei1981@gmail.com>
2021-04-26 16:15:13 +0800

    Module optimization

---
+++ pallets/nft-auction/src/default_weight.rs
@@ -0,0 +1,30 @@
+//! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 2.0.0
+
+#![allow(unused_parens)]
+#![allow(unused_imports)]
+
+use frame_support::weights::{constants::RocksDbWeight as DbWeight, Weight};
+
+impl crate::WeightInfo for () {
+    fn create_auction() -> Weight {
+        (66_234_000 as Weight)
+            .saturating_add(DbWeight::get().reads(5 as Weight))
+            .saturating_add(DbWeight::get().writes(3 as Weight))
+    }
+    fn bid() -> Weight {
+        (66_234_000 as Weight)
+            .saturating_add(DbWeight::get().reads(5 as Weight))
+            .saturating_add(DbWeight::get().writes(3 as Weight))
+    }
+    fn cancel_auction() -> Weight {
+        (66_234_000 as Weight)
+            .saturating_add(DbWeight::get().reads(5 as Weight))
+            .saturating_add(DbWeight::get().writes(3 as Weight))
+    }
+
+    fn finish_auction() -> Weight {
+        (66_234_000 as Weight)
+            .saturating_add(DbWeight::get().reads(5 as Weight))
+            .saturating_add(DbWeight::get().writes(3 as Weight))
+    }
+}

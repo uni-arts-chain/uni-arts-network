f1705a233e25f05f693835c9665919bcff4d81a6
terry.tu <tuminfei1981@gmail.com>
2021-05-12 16:30:42 +0800

    migrate

--- pallets/nft-multi/src/lib.rs
+++ pallets/nft-multi/src/lib.rs
@@ -225,6 +225,65 @@ pub struct VestingItem<AccountId, Moment> {
 
 #[derive(Encode, Decode, Default, Clone, PartialEq)]
 #[cfg_attr(feature = "std", derive(Debug))]
+pub struct SaleOrderV1<AccountId> {
+    pub order_id: u64,
+    pub collection_id: u64,
+    pub item_id: u64,
+    pub value: u64,
+    pub owner: AccountId,
+    pub price: u64, // maker order's price\
+}
+
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub struct SplitSaleOrderV1<AccountId> {
+    pub order_id: u64,
+    pub collection_id: u64,
+    pub item_id: u64,
+    pub value: u64,
+    pub balance: u64,
+    pub owner: AccountId,
+    pub price: u64, // maker order's price\
+}
+
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub struct SaleOrderHistoryV1<AccountId, BlockNumber> {
+    pub collection_id: u64,
+    pub item_id: u64,
+    pub value: u64,
+    pub seller: AccountId,
+    pub buyer: AccountId,
+    pub price: u64,
+    pub buy_time: BlockNumber,
+}
+
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub struct AuctionV1<AccountId, BlockNumber> {
+    pub id: u64,
+    pub collection_id: u64,
+    pub item_id: u64,
+    pub value: u64,
+    pub owner: AccountId,
+    pub start_price: u64,
+    pub increment: u64,
+    pub current_price: u64,
+    pub start_time: BlockNumber,
+    pub end_time: BlockNumber,
+}
+
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub struct BidHistoryV1<AccountId, BlockNumber> {
+    pub auction_id: u64,
+    pub bidder: AccountId,
+    pub bid_price: u64,
+    pub bid_time: BlockNumber,
+}
+
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+#[cfg_attr(feature = "std", derive(Debug))]
 pub struct SaleOrder<AccountId> {
     pub order_id: u64,
     pub collection_id: u64,
@@ -1544,7 +1603,7 @@ impl<T: Trait> Module<T> {
             // pub AuctionList get(fn get_auction): double_map hasher(blake2_128_concat) u64, hasher(blake2_128_concat) u64 => Auction<T::AccountId, T::BlockNumber>;
             //
             // pub BidHistoryList get(fn bid_history_list): map hasher(identity) u64 => Vec<BidHistory<T::AccountId, T::BlockNumber>>;
-            SaleOrderList::<T>::translate::<SaleOrder<T::AccountId>, _>(|_, _, p: SaleOrder<T::AccountId>|{
+            SaleOrderList::<T>::translate::<SaleOrderV1<T::AccountId>, _>(|_, _, p: SaleOrderV1<T::AccountId>|{
                 let new_data: SaleOrder<T::AccountId> = SaleOrder {
                     order_id: p.order_id,
                     collection_id: p.collection_id,
@@ -1557,7 +1616,7 @@ impl<T: Trait> Module<T> {
                 Some(new_data)
             });
 
-            SaleOrderByIdList::<T>::translate::<SaleOrder<T::AccountId>, _>(|_, p: SaleOrder<T::AccountId>|{
+            SaleOrderByIdList::<T>::translate::<SaleOrderV1<T::AccountId>, _>(|_, p: SaleOrderV1<T::AccountId>|{
                 let new_data: SaleOrder<T::AccountId> = SaleOrder {
                     order_id: p.order_id,
                     collection_id: p.collection_id,
@@ -1570,7 +1629,7 @@ impl<T: Trait> Module<T> {
                 Some(new_data)
             });
 
-            SeparableSaleOrder::<T>::translate::<SplitSaleOrder<T::AccountId>, _>(|_, p: SplitSaleOrder<T::AccountId>|{
+            SeparableSaleOrder::<T>::translate::<SplitSaleOrderV1<T::AccountId>, _>(|_, p: SplitSaleOrderV1<T::AccountId>|{
                 let new_data: SplitSaleOrder<T::AccountId> = SplitSaleOrder {
                     order_id: p.order_id,
                     collection_id: p.collection_id,
@@ -1584,7 +1643,7 @@ impl<T: Trait> Module<T> {
                 Some(new_data)
             });
 
-            AuctionList::<T>::translate::<Auction<T::AccountId, T::BlockNumber>, _>(|_, _, p: Auction<T::AccountId, T::BlockNumber>|{
+            AuctionList::<T>::translate::<AuctionV1<T::AccountId, T::BlockNumber>, _>(|_, _, p: AuctionV1<T::AccountId, T::BlockNumber>|{
                 let new_data: Auction<T::AccountId, T::BlockNumber> = Auction {
                     id: p.id,
                     collection_id: p.collection_id,
@@ -1601,7 +1660,7 @@ impl<T: Trait> Module<T> {
                 Some(new_data)
             });
 
-            HistorySaleOrderList::<T>::translate::<Vec<SaleOrderHistory<T::AccountId, T::BlockNumber>>, _>(|_, _, pslist| Some(
+            HistorySaleOrderList::<T>::translate::<Vec<SaleOrderHistoryV1<T::AccountId, T::BlockNumber>>, _>(|_, _, pslist| Some(
                 pslist
                     .into_iter()
                     .map(|history| SaleOrderHistory {
@@ -1617,7 +1676,7 @@ impl<T: Trait> Module<T> {
                     .collect::<Vec<_>>()
             ));
 
-            BidHistoryList::<T>::translate::<Vec<BidHistory<T::AccountId, T::BlockNumber>>, _>(|_, pslist| Some(
+            BidHistoryList::<T>::translate::<Vec<BidHistoryV1<T::AccountId, T::BlockNumber>>, _>(|_, pslist| Some(
                 pslist
                     .into_iter()
                     .map(|history| BidHistory {

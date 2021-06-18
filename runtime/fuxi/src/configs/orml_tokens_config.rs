08ac43df949c94274ac28dd95cdd1a66213a9823
terry.tu <tuminfei1981@gmail.com>
2021-06-18 10:27:41 +0800

    update

--- runtime/fuxi/src/configs/orml_tokens_config.rs
+++ runtime/fuxi/src/configs/orml_tokens_config.rs
@@ -2,6 +2,14 @@ use orml_tokens::Config;
 use uniarts_primitives::*;
 use crate::*;
 use crate::weights::orml_tokens::WeightInfo;
+use orml_traits::parameter_type_with_key;
+use sp_runtime::traits::Zero;
+
+parameter_type_with_key! {
+	pub ExistentialDeposits: |_currency_id: CurrencyId| -> Balance {
+		Zero::zero()
+	};
+}
 
 impl Config for Runtime {
     type Event = Event;
